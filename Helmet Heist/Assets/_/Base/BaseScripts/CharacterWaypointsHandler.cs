1f94c5784a15fd1e47066bc02c68f00ed383aee4
noid <noraliao508@gmail.com>
2024-12-10 16:31:31 -0800

    sound

--- Helmet Heist/Assets/_/Base/BaseScripts/CharacterWaypointsHandler.cs
+++ Helmet Heist/Assets/_/Base/BaseScripts/CharacterWaypointsHandler.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 using V_AnimationSystem;
 using CodeMonkey.Utils;
 
-/*
- * Character moves between waypoints
- * */
-public class CharacterWaypointsHandler : MonoBehaviour {
-        
+public class CharacterWaypointsHandler : MonoBehaviour
+{
+
     private const float speed = 25f;
 
     [SerializeField] private List<Vector3> waypointList;
@@ -27,13 +25,17 @@ public class CharacterWaypointsHandler : MonoBehaviour {
     [SerializeField] private float fov = 90f;
     [SerializeField] private float viewDistance = 50f;
 
+    [SerializeField] private AudioSource audioSource; // 音效播放器
+    [SerializeField] private AudioClip killSound; // 杀死主角的音效
+
     private FieldOfView fieldOfView;
 
     private V_UnitSkeleton unitSkeleton;
     private V_UnitAnimation unitAnimation;
     private AnimatedWalker animatedWalker;
 
-    private enum State {
+    private enum State
+    {
         Waiting,
         Moving,
         AttackingPlayer,
@@ -47,7 +49,8 @@ public class CharacterWaypointsHandler : MonoBehaviour {
 
     public Animator spriteAnim;
 
-    private void Start() {
+    private void Start()
+    {
         Transform bodyTransform = transform.Find("Body");
         unitSkeleton = new V_UnitSkeleton(1f, bodyTransform.TransformPoint, (Mesh mesh) => bodyTransform.GetComponent<MeshFilter>().mesh = mesh);
         unitAnimation = new V_UnitAnimation(unitSkeleton);
@@ -60,27 +63,28 @@ public class CharacterWaypointsHandler : MonoBehaviour {
         fieldOfView = Instantiate(pfFieldOfView, null).GetComponent<FieldOfView>();
         fieldOfView.SetFoV(fov);
         fieldOfView.SetViewDistance(viewDistance);
-
-
     }
 
-    private void Update() {
-        switch (state) {
-        default:
-        case State.Waiting:
-        case State.Moving:
-            HandleMovement();
-            FindTargetPlayer();
-            break;
-        case State.AttackingPlayer:
-            AttackPlayer();
-            break;
-        case State.Busy:
-            break;
+    private void Update()
+    {
+        switch (state)
+        {
+            default:
+            case State.Waiting:
+            case State.Moving:
+                HandleMovement();
+                FindTargetPlayer();
+                break;
+            case State.AttackingPlayer:
+                AttackPlayer();
+                break;
+            case State.Busy:
+                break;
         }
         unitSkeleton.Update(Time.deltaTime);
 
-        if (fieldOfView != null) {
+        if (fieldOfView != null)
+        {
             fieldOfView.SetOrigin(transform.position);
             fieldOfView.SetAimDirection(GetAimDir());
         }
@@ -88,33 +92,32 @@ public class CharacterWaypointsHandler : MonoBehaviour {
         Debug.DrawLine(transform.position, transform.position + GetAimDir() * 10f);
     }
 
-    private void FindTargetPlayer() {
-        if (Vector3.Distance(GetPosition(), player.GetPosition()) < viewDistance) {
-            // Player inside viewDistance
+    private void FindTargetPlayer()
+    {
+        if (Vector3.Distance(GetPosition(), player.GetPosition()) < viewDistance)
+        {
             Vector3 dirToPlayer = (player.GetPosition() - GetPosition()).normalized;
-            if (Vector3.Angle(GetAimDir(), dirToPlayer) < fov / 2f) {
-                // Player inside Field of View
+            if (Vector3.Angle(GetAimDir(), dirToPlayer) < fov / 2f)
+            {
                 RaycastHit2D raycastHit2D = Physics2D.Raycast(GetPosition(), dirToPlayer, viewDistance);
-                if (raycastHit2D.collider != null) {
-                    // Hit something
-
-                    //ADD GAME OVER HERE AND TAKE OUT SHOOTING
-                    if (raycastHit2D.collider.gameObject.GetComponent<Player>() != null) {
-                        // Hit Player
+                if (raycastHit2D.collider != null)
+                {
+                    if (raycastHit2D.collider.gameObject.GetComponent<Player>() != null)
+                    {
                         StartAttackingPlayer();
-                    } else {
-                        // Hit something else
                     }
                 }
             }
         }
     }
 
-    public void StartAttackingPlayer() {
+    public void StartAttackingPlayer()
+    {
         AttackPlayer();
     }
 
-    private void AttackPlayer() {
+    private void AttackPlayer()
+    {
         state = State.Busy;
 
         Vector3 targetPosition = player.GetPosition();
@@ -122,61 +125,90 @@ public class CharacterWaypointsHandler : MonoBehaviour {
         lastMoveDir = dirToTarget;
 
         unitAnimation.PlayAnimForced(attackUnitAnim, dirToTarget, 2f, (UnitAnim unitAnim) => {
-            // Attack complete
-            if (player.IsDead()) {
+            if (player.IsDead())
+            {
                 state = State.Moving;
-            } else {
+            }
+            else
+            {
                 state = State.AttackingPlayer;
             }
         }, (string trigger) => {
-            // Damage Player
+            // 造成伤害
             player.Damage(this);
+
+            if (player.IsDead())
+            {
+                // 播放杀死主角的音效
+                PlayKillSound();
+            }
         }, null);
-        
+
         Vector3 gunEndPointPosition = unitSkeleton.GetBodyPartPosition("MuzzleFlash");
         Shoot_Flash.AddFlash(gunEndPointPosition);
         WeaponTracer.Create(gunEndPointPosition, player.GetPosition());
     }
 
-    private void HandleMovement() {
-        switch (state) {
-        case State.Waiting:
-                spriteAnim.SetTrigger( "Idle" );
-                waitTimer -= Time.deltaTime;
-            animatedWalker.SetMoveVector(Vector3.zero);
-            if (waitTimer <= 0f) {
-                state = State.Moving;
+    private void PlayKillSound()
+    {
+        if (audioSource != null && killSound != null)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.clip = killSound; // 设置音效
+                audioSource.Play(); // 播放音效
             }
-            break;
-        case State.Moving:
-            Vector3 waypoint = waypointList[waypointIndex];
-
-            Vector3 waypointDir = (waypoint - transform.position).normalized;
-            lastMoveDir = waypointDir;
-
-            float distanceBefore = Vector3.Distance(transform.position, waypoint);
-            spriteAnim.SetTrigger("Walk");
-            //animatedWalker.SetMoveVector(waypointDir);
-            transform.position = transform.position + waypointDir * speed * Time.deltaTime;
-            float distanceAfter = Vector3.Distance(transform.position, waypoint);
-
-            float arriveDistance = .1f;
-            if (distanceAfter < arriveDistance || distanceBefore <= distanceAfter) {
-                // Go to next waypoint
-                waitTimer = waitTimeList[waypointIndex];
-                waypointIndex = (waypointIndex + 1) % waypointList.Count;
-                state = State.Waiting;
-            }
-            break;
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource or KillSound is not assigned!");
         }
     }
 
-    public Vector3 GetPosition() {
+    private void HandleMovement()
+    {
+        switch (state)
+        {
+            case State.Waiting:
+                spriteAnim.SetTrigger("Idle");
+                waitTimer -= Time.deltaTime;
+                animatedWalker.SetMoveVector(Vector3.zero);
+                if (waitTimer <= 0f)
+                {
+                    state = State.Moving;
+                }
+                break;
+            case State.Moving:
+                Vector3 waypoint = waypointList[waypointIndex];
+
+                Vector3 waypointDir = (waypoint - transform.position).normalized;
+                lastMoveDir = waypointDir;
+
+                float distanceBefore = Vector3.Distance(transform.position, waypoint);
+                spriteAnim.SetTrigger("Walk");
+                transform.position = transform.position + waypointDir * speed * Time.deltaTime;
+                float distanceAfter = Vector3.Distance(transform.position, waypoint);
+
+                float arriveDistance = .1f;
+                if (distanceAfter < arriveDistance || distanceBefore <= distanceAfter)
+                {
+                    waitTimer = waitTimeList[waypointIndex];
+                    waypointIndex = (waypointIndex + 1) % waypointList.Count;
+                    state = State.Waiting;
+                }
+                break;
+        }
+    }
+
+    public Vector3 GetPosition()
+    {
         return transform.position;
     }
 
-    public Vector3 GetAimDir() {
+    public Vector3 GetAimDir()
+    {
         return lastMoveDir;
     }
 
 }
+
